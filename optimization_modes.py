a4844f876b8bb5147415b3f67dd32ac695ee6343
Prasoon Pandey <pandey.prasoon2000@gmail.com>
2024-03-27 17:04:49 +0100

    small changes

--- optimization_modes.py
+++ optimization_modes.py
@@ -2,11 +2,11 @@ from mlgw_bns.hyperparameter_optimization import HyperparameterOptimization
 from mlgw_bns.model import *
 
 def hyper_opt_fun():
-    main_model = ModesModel(filename = "pp_small_default", modes=[Mode(2,2)])
+    main_model = ModesModel(filename = "pp_small_default", modes=[Mode(3,3)])
     main_model.load()
-    mmodel = main_model.models[Mode(2, 2)]
+    mmodel = main_model.models[Mode(3, 3)]
     ho = HyperparameterOptimization(mmodel)
-    ho.optimize_and_save(0.1)
+    ho.optimize_and_save(0.3)
 
 
 if __name__ == "__main__":
